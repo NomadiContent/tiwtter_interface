cf5064dc864e455220d0f8175b4f12cb2529f309
Charley Montgomery <charleymontgomery@Charleys-MacBook-Pro.local>
2017-11-15 14:50:16 +0800

    updating UI

--- js/app.js
+++ js/app.js
@@ -5,41 +5,57 @@ const app = express();
 let tweets = [];
 let friends = [];
 let messages = [];
-
+let profilePhoto = "";
+//
 app.use('/static', express.static('public'));
-
+//
 app.set('view engine', 'pug');
+//
 
-function myTweets() {
+app.get('/', (req, res) => {
   config.keys.get('statuses/user_timeline', { screen_name: 'clmontgo' },  function (err, data, response) {
+
+    res.locals.profilePhoto =  data[0]['user']['profile_image_url'];
 
     for (var i = 0; i < 5; i++) {
       tweets.push(data[i].text);
     }
-    console.log(data);
+    res.locals.tweets = tweets;
+    //res.locals.name = tweets;
+
+    config.keys.get('friends/list', { screen_name: 'clmontgo' },  function (err, data, response) {
+      //console.log('second')
+      for (var i = 0; i < 5; i++) {
+        friends.push(data['users'][i]['name']);
+        }
+
+      res.locals.friends = friends;
 
 
-  })
-}
+        config.keys.get('direct_messages', { screen_name: 'clmontgo' },  function (err, data, response) {
 
-myTweets();
+          //for (var i = 0; i < 5; i++) {
+          //  console.log('entered')
+          //  if (i > messages.length - 1) {
+          //    break;
+          //    console.log('exit')
+          //  } else {
+          //    console.log('not exit')
+          //    messages.push(data[i]['text']);
+          //  }
+          //}
 
-//app.use('/', () => {
+          //messages = data['text']
+          console.log(messages)
+          res.locals.messages = messages;
 
-//});
-
-app.get('/', (req, res) => {
-  config.keys.get('statuses/user_timeline', { screen_name: 'clmontgo' },  function (err, data, response) {
-    res.locals.tweets = [];
-
-    for (var i = 0; i < 5; i++) {
-      res.locals.tweets.push(data[i].text);
-    }
-
-    res.render('index');
-
+          res.render('index');
+          tweets = [];
+          friends = [];
+          messages = [];
+      });
+    });
   });
-
 });
 
 
